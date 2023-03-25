5c35bc8ad778c4408991acfb95b1822abcb69578
jesusudiz <jesusgabrieludiz@gmail.com>
2023-03-25 19:48:43 -0300

    commit objeto

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 function laCajaDePandora(numero){
-// proximamente escribiremos codigo aqui
-}
+    // proximamente escribiremos codigo aqui
+
+    if(numero % 2 ===0){
+        return Number(numero.toString(2))
+    }else {
+        return Number(numero.toString(16))
+    }
+    }
+
+    function colaborador (){
+        return {
+         nombre:"jesus",
+         edad:"35",
+         nacionalidad:"Venezolano"
+        }
+      }
