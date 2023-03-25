738953515c07652a6e0c4018315bfc2ceab2df73
andrecarosu <andrecarosu@hotmail.com>
2023-03-25 17:09:28 -0500

    cambio Carolina

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 function laCajaDePandora(numero){
 // proximamente escribiremos codigo aqui
+
+if (numero % 2 === 0) {
+    // Si es un número par, convertirlo a binario y retornarlo
+    return numero.toString(2);
+  } else {
+    // Si es un número impar, convertirlo a hexadecimal y retornarlo
+    return numero.toString(16);
+  }
+}
+
+
+
+function carolina() {
+    return {
+        nombre: 'carolina',
+        edad: 42,
+        nacionalidad: 'colombia',
+        
+    }
 }
